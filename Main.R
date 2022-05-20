6ad9910c9031da552128b9c1edcc48479a80f114
StanDoge <StanlyDoge@gmail.com>
2022-05-19 19:40:04 -0600

    Systematic sampling indexes

--- Main.R
+++ Main.R
@@ -14,8 +14,6 @@ systematic.sample = function(N,n){
   r = sample(1:k, 1) # conteo para hacer el "slide"
 
   sys.samp = seq(r, r + k*(n-1), k) # obtencion de indice de muestras
-
-  cat("The selected systematic sample is: \"", sys.samp, "\"\n")
 }
 
 # Tomando de 3 en 3 se obtienen 8,400 ; cumpliendo el 10% aprox solicitado
@@ -24,4 +22,6 @@ sample_size = 28000
 sys.samp = seq(sample_index, sample_index + k*(sample_size - 1), k)
 
 systematic_sample = systematic.sample(poblation,sample_size)
+
+
 
