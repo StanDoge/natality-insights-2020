60022370d55ef542ed987174afbb550cbb0a5706
StanDoge <StanlyDoge@gmail.com>
2022-05-19 18:59:04 -0600

    Parameter definition

--- Main.R
+++ Main.R
@@ -1,3 +1,9 @@
 library("readxl")
 datasource = read_excel('./DB nacimientos 2020.xlsx')
 sample (datasource)
+
+sampling_interval = 2
+
+# Tomando de 2 en 2 se obtienen 8,000 ; cumpliendo el 10% aprox solicitado
+sample_size = 20000
+
