67e05a1233e030dda4f54476ff25f1decb7606b7
StanDoge <StanlyDoge@gmail.com>
2022-05-19 19:04:27 -0600

    Library addition, plyr

--- Main.R
+++ Main.R
@@ -1,9 +1,12 @@
 library("readxl")
+library("plyr")
 datasource = read_excel('./DB nacimientos 2020.xlsx')
 sample (datasource)
 
 sampling_interval = 2
+poblation = data.frame(table(datasource))
 
 # Tomando de 2 en 2 se obtienen 8,000 ; cumpliendo el 10% aprox solicitado
 sample_size = 20000
 
+# k = ceiling(poblation/sample_size)
