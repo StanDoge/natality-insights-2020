393ae10e5d39ed09cb75c1d01f88bdce2540b044
StanDoge <StanlyDoge@gmail.com>
2022-05-19 21:33:30 -0600

    Fix: sample_size's value change it

--- Main.R
+++ Main.R
@@ -5,7 +5,7 @@ datasource = read_excel('./DB nacimientos 2020.xlsx')
 sample (datasource)
 
 poblation = nrow(datasource)
-sample_size = 28000
+sample_size = 8315
 
 # Funcion para obtener indices de valores de manera sistematica
 systematic.sample = function(N,n){
@@ -14,12 +14,9 @@ systematic.sample = function(N,n){
   r = sample(1:k, 1) # conteo para hacer el "slide"
 
   sys.samp = seq(r, r + k*(n-1), k) # obtencion de indice de muestras
-
-  # cat("The selected systematic sample is: \"", sys.samp, "\"\n")
-
 }
 
 # Tomando de 3 en 3 se obtienen 8,400 ; cumpliendo el 10% aprox solicitado
-systematic_sample = systematic.sample(poblation,sample_size)
+systematic_sample = datasource[systematic.sample(poblation,sample_size),]
 
-# df.sample = datasource[datasource$id==systematic_sample] todo: delimitar el df original de 80,000 -> 8,
+systematic_sample
