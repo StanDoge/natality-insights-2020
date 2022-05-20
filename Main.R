9421f7079414944cd8a580f487e536de925ad119
StanDoge <StanlyDoge@gmail.com>
2022-05-19 21:36:15 -0600

    Comments to systematic_sample var

--- Main.R
+++ Main.R
@@ -16,7 +16,6 @@ systematic.sample = function(N,n){
   sys.samp = seq(r, r + k*(n-1), k) # obtencion de indice de muestras
 }
 
-# Tomando de 3 en 3 se obtienen 8,400 ; cumpliendo el 10% aprox solicitado
+# Tomando de 10 en 10 se obtienen 8,315 ; cumpliendo el 10% aprox solicitado
 systematic_sample = datasource[systematic.sample(poblation,sample_size),]
 
-systematic_sample
