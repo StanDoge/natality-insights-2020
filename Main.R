3aa066eb28be04be6e442d618727ca4d22610fd9
StanDoge <StanlyDoge@gmail.com>
2022-05-19 19:21:20 -0600

    Systematic sampling function added

--- Main.R
+++ Main.R
@@ -1,12 +1,23 @@
 library("readxl")
 library("plyr")
+
 datasource = read_excel('./DB nacimientos 2020.xlsx')
 sample (datasource)
 
 sampling_interval = 2
-poblation = data.frame(table(datasource))
+poblation = nrow(datasource)
 
-# Tomando de 2 en 2 se obtienen 8,000 ; cumpliendo el 10% aprox solicitado
-sample_size = 20000
+systematic.sample = function(N,n){
+  k = ceiling(N/n)
+  r = sample(1:k, 1)
+  sys.samp = seq(r, r + k*(n-1), k)
+  cat("The selected systematic sample is: \"", sys.samp, "\"\n")
+}
 
-# k = ceiling(poblation/sample_size)
+# Tomando de 3 en 3 se obtienen 8,400 ; cumpliendo el 10% aprox solicitado
+sample_size = 28000
+
+sys.samp = seq(sample_index, sample_index + k*(sample_size - 1), k)
+
+systematic_sample = systematic.sample(poblation,sample_size)
+
