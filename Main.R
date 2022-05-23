55239e92c9f4e2fde6a1a851caa7a61e6b891810
FatimaAlexandra <fathykztillo@gmail.com>
2022-05-22 21:56:50 -0600

    Update Main.R
    Medidas de posición actualizadas por Fatima

--- Main.R
+++ Main.R
@@ -22,7 +22,38 @@ muestra_sistematica <- datos[muestra.sistematica(poblacion,n_muestra),]
 
 
 # --------------------------------------- medidas tendencia central --------------------------------------------------
+#Medidas de posición      peso_nac
+#1 Qu. media 3Qu. Mediana 
+summary(DB_nacimientos_2020$peso_nac )
+#moda
+frecuencias <- data.frame(table(DB_nacimientos_2020$peso_nac))
+moda <- frecuencias[which.max(frecuencias$Freq),1]
+paste("La moda de la variable peso es", moda)
 
+#Medidas de posición    madre_edad
+#1 Qu. media 3Qu. Mediana 
+summary(DB_nacimientos_2020$madre_edad)
+#moda
+frecuencias <- data.frame(table(DB_nacimientos_2020$madre_edad))
+moda <- frecuencias[which.max(frecuencias$Freq),1]
+paste("La moda de la variable edad de madre  es", moda)
+
+#Medidas de posición    semanas_gestacion
+#1 Qu. media 3Qu. Mediana 
+summary(DB_nacimientos_2020$semana_gestacion)
+#moda
+frecuencias <- data.frame(table(DB_nacimientos_2020$semana_gestacion))
+moda <- frecuencias[which.max(frecuencias$Freq),1]
+paste("La moda de la variable semanas de gestación  es", moda)
+
+# --------------------------------------- medidas tendencia central --------------------------------------------------
+
+
+# --------------------------------------- medidas dispersion ---------------------------------------------------------
+# --------------------------------------- medidas dispersion ---------------------------------------------------------
+
+
+# --------------------------------------- graficas --------------------------------------------------------------------
 hist(muestra_sistematica$semana_gestacion)
 
 hist(muestra_sistematica$semana_gestacion, main="Histograma de las semanas de gestacion", xlab="Semanas",
@@ -33,14 +64,5 @@ x <- seq(min(muestra_sistematica$semana_gestacion), max(muestra_sistematica$sema
 
 f <- dnorm(x, mean <- mean(muestra_sistematica$semana_gestacion), sd = sd(muestra_sistematica$semana_gestacion))
 lines(x, f, col <- "red", lwd = 2)
-
-# --------------------------------------- medidas tendencia central --------------------------------------------------
-
-
-# --------------------------------------- medidas dispersion ---------------------------------------------------------
-# --------------------------------------- medidas dispersion ---------------------------------------------------------
-
-
-# --------------------------------------- graficas --------------------------------------------------------------------
 # --------------------------------------- graficas---------------------------------------------------------------------
 
